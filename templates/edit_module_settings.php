105e354b845e9c5b1f751da806330bb8f3e297f7
Are Antonsen <antonar@gmx.com>
2015-11-27 23:01:38 +0100

    Lagt til form og knapper i edit_module_settings.php.

--- templates/edit_module_settings.php
+++ templates/edit_module_settings.php
@@ -7,12 +7,14 @@
     <form action="" method="post">
         <div class="row">
             <div class="large-8 columns text-right">
-                <label for="standardAll" class="right">Angi standard modullengde for alle moduler: </label>
+                <label for="standardAll">Angi standard modullengde for alle moduler: </label>
             </div>
             <div class="large-2 columns">
                 <input id="standardAll" type="number" value="30">
             </div>
-            <div class="large-2 columns"> timer</div>
+            <div class="large-2 columns">
+                <label for="standardAll"> timer</label>
+            </div>
         </div>
         <hr />
         <?php foreach ($modules as $m): ?>
@@ -25,10 +27,10 @@
                 </div>
                 <div class="large-2 columns">
                     <?php if($m->index == (count($modules)-1)): ?>
-                        <button class="button small" id="abort">Avbryt</button>
+                        <button class="button small secondary" id="cancel">Avbryt</button>
                     <?php endif ?>
                     <?php if($m->index == count($modules)): ?>
-                        <button class="button small" id="save_module_settings">Lagre</button>
+                        <button type="submit" class="button small success" id="save">Lagre</button>
                     <?php endif ?>
                 </div>
             </div>
