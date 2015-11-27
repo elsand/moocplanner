6fd740051c0b4215733be7baf5a8c8498c4a158d
Are Antonsen <antonar@gmx.com>
2015-11-27 13:47:35 +0100

    footer.php: avrunding av prosenttall.

--- templates/footer.php
+++ templates/footer.php
@@ -11,7 +11,7 @@
                 <dt>Følgende moduler er ikke fullt booket i kalenderen:</dt>
                 <div class="footer_module_list">
                     <?php foreach ($not_fully_booked as $m): ?>
-                        <dd><?= "Modul " . $m->index . ": " . $m->name . " " . ($m->booked_hours / $m->estimated_hours) * 100 . "%" . " (" . $m->booked_hours . " av " . $m->estimated_hours . " timer)"; ?></dd>
+                        <dd><?= "Modul " . $m->index . ": " . $m->name . " " . floor(($m->booked_hours / $m->estimated_hours) * 100) . "%" . " (" . $m->booked_hours . " av " . $m->estimated_hours . " timer)"; ?></dd>
                     <?php endforeach ?>
                 </div>
             </dl>
